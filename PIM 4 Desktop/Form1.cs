30f710be97d075511383ddc4cea2f22c020362a0
Breno B. Leonetti <brenoleonetti@gmail.com>
2021-10-23 23:29:08 -0300

    Setimo Commit 23 10 21

--- PIM 4 Desktop/Form1.cs
+++ PIM 4 Desktop/Form1.cs
@@ -29,7 +29,7 @@ namespace PIM_4_Desktop
 
         private void btnEntrar_Click(object sender, EventArgs e)
         {
-            var form = new Form18();
+            var form = new Form17();
             this.Hide();
             form.Show();
         }
